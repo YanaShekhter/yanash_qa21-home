d313cf9783905c8c0810ecc4a2ab2fe70d168e12
YanaShekhter <51967134+YanaShekhter@users.noreply.github.com>
2019-08-25 18:16:24 +0300

    Add module "wikipedia-web-tests".
    Task:
    Create module "wikipedia-web-tests";
     In the module create class "SearchWikiTest"
    1. open wikipedia (https://en.wikipedia.org)
    2. type in the input field text;
    //code
    package homeCom.telran.selenium;
    
    import org.openqa.selenium.By;
    import org.openqa.selenium.WebDriver;
    import org.openqa.selenium.chrome.ChromeDriver;
    import org.testng.annotations.Test;
    
    @Test
    public class HW01WikiSearch {
        WebDriver driver;
        public void searchWiki() throws InterruptedException {
            driver = new ChromeDriver();
            driver.get("https://en.wikipedia.org/wiki/Main_Page");
    
            driver.findElement(By.name("search")).click();
            driver.findElement(By.name("search")).clear();
            driver.findElement(By.name("search")).sendKeys("java");
    
            driver.findElement(By.name("go")).click();
    
            Thread.sleep(5000);
            driver.quit();
        }
    }

--- first-project-home/src/test/java/homeCom/telran/selenium/HW01WikiSearch.java
+++ first-project-home/src/test/java/homeCom/telran/selenium/HW01WikiSearch.java
@@ -10,11 +10,11 @@ public class HW01WikiSearch {
     WebDriver driver;
     public void searchWiki() throws InterruptedException {
         driver = new ChromeDriver();
-        driver.get("https://en.wikipedia.org/wiki/Main_Page");
-
+        driver.get("https://en.wikipedia.org/wiki/Main_Page"); // with URL https://en.wikipedia.org the code is not working, issue with my browser.
         driver.findElement(By.name("search")).click();
         driver.findElement(By.name("search")).clear();
         driver.findElement(By.name("search")).sendKeys("java");
+
 
         driver.findElement(By.name("go")).click();
 
